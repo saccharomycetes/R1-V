38d2186224048d658223702de5880aaa91b921b5
saccharomycetes <jzhang37@usc.edu>
2025-08-27 02:05:58 +0000

    update

--- setup.sh
+++ setup.sh
@@ -3,13 +3,18 @@ cd src/r1-v
 pip install -e ".[dev]"
 
 # Addtional modules
-pip install wandb==0.18.3
+pip install wandb
 pip install tensorboardx
 pip install qwen_vl_utils torchvision
-pip install flash-attn --no-build-isolation
 
-# vLLM support 
-pip install vllm==0.7.2
+# Ensure correct torch version
+pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1
+
+# Flash attention - specify version
+pip install flash-attn==2.8.0.post2 --no-build-isolation
+
+# vLLM support - specify version
+pip install vllm==0.10.1.1
 
 # fix transformers version
 pip install git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
